8d4cc2d14a0a35f715d933ad47a7d0ddf1bc5cf2
Baeo Maltinksy <baeo.maltinsky@gmail.com>
2019-08-28 16:41:16 -0700

    Working

--- examples/1d.rs
+++ examples/1d.rs
@@ -1,6 +1,7 @@
 use lazy_static::*;
-use rand::distributions::Distribution;
+use rand::{distributions::Distribution, SeedableRng};
 use rand_distr::Normal;
+use rand_xorshift::XorShiftRng;
 use tdmc::*;
 
 const TIMESTEP: f64 = 0.0001;
@@ -15,7 +16,7 @@ struct Drift {}
 impl TDMC for Drift {
     type State = f64;
 
-    fn propagate_sample(x: &mut Self::State, _: i32) {
+    fn propagate_sample(&mut self, x: &mut Self::State, _: i32) {
         *x += 0.1;
     }
 
@@ -24,18 +25,27 @@ impl TDMC for Drift {
     }
 }
 
-struct Brownian {}
+struct Brownian {
+    rng: XorShiftRng,
+}
+
+impl Brownian {
+    fn new() -> Self {
+        Brownian {
+            rng: XorShiftRng::from_rng(rand::thread_rng()).unwrap(),
+        }
+    }
+}
 
 impl TDMC for Brownian {
     type State = f64;
 
-    fn propagate_sample(x: &mut Self::State, _: i32) {
+    fn propagate_sample(&mut self, x: &mut Self::State, _: i32) {
         lazy_static! {
             static ref BROWNIAN_INCREMENT: Normal<f64> =
                 Normal::new(0.0, (2.0 * TIMESTEP).sqrt()).unwrap();
         }
-        let mut rng = rand::thread_rng();
-        *x += (*BROWNIAN_INCREMENT).sample(&mut rng);
+        *x += (*BROWNIAN_INCREMENT).sample(&mut self.rng);
     }
 
     fn chi(x_new: &Self::State, x_old: &Self::State, _: i32) -> f64 {
@@ -44,23 +54,25 @@ impl TDMC for Brownian {
 }
 
 fn main() {
-    let end_walker_data = Drift::run_tdmc(0.0, 1, 5);
+    let mut drift = Drift {};
+    let end_walker_data = drift.run_tdmc(0.0, 1, 5);
     for walker_data in end_walker_data {
         print!("{} ", walker_data.0);
     }
     println!();
 
-    let n_replicates = 10_000;
+    let n_replicates = 1000;
     let n_timesteps = (1. / TIMESTEP) as i32;
 
-    let end_walker_data = Brownian::run_tdmc(0.0, n_timesteps, n_replicates);
-    println!("Final walker numner: {}", end_walker_data.len());
+    let mut brownian = Brownian::new();
+    let end_walker_data = brownian.run_tdmc(0.0, n_timesteps, n_replicates);
+    println!("Final walker number: {}", end_walker_data.len());
     println!(
         "Average final walkers per initial walker: {}",
         end_walker_data.len() as i32 / n_replicates
     );
-    for walker_data in end_walker_data {
-        print!("{} ", walker_data.0);
-    }
+    //for walker_data in end_walker_data {
+    //    print!("{} ", walker_data.0);
+    //}
     println!();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,35 @@
 use im_rc::Vector as ImVector;
-use rand::distributions::{Distribution, Uniform};
+use rand::{
+    distributions::{Distribution, Uniform},
+    SeedableRng,
+};
+use rand_xorshift::XorShiftRng;
+use std::time::Instant;
 
 pub trait TDMC: Sized {
     type State: Clone;
 
-    fn propagate_sample(state: &mut Self::State, timestamp: i32);
+    fn propagate_sample(&mut self, state: &mut Self::State, timestamp: i32);
 
-    fn chi(x: &Self::State, y: &Self::State, timestamp: i32) -> f64;
+    fn chi(new: &Self::State, old: &Self::State, timestamp: i32) -> f64;
 
     fn run_tdmc(
+        &mut self,
         initial_walker_state: Self::State,
         n_dmc_steps: i32,
         n_initial_walkers: i32,
     ) -> ImVector<(Self::State, i32)> {
         println!("Beginning TDMC.");
 
+        // Initialize clock.
+        let start_clock = Instant::now();
         let mut n_dynamics_evaluations = 0;
 
-        let mut rng = rand::thread_rng();
+        // Initialize RNG.
+        let mut rng = XorShiftRng::from_rng(rand::thread_rng()).unwrap();
         let uniform = Uniform::new_inclusive(0f64, 1f64);
 
+        // Create a list of walkers with uniformly distributed tickets.
         let mut walker_list: ImVector<WalkerData<Self::State>> = (0..n_initial_walkers)
             .zip((&uniform).sample_iter(&mut rng))
             .map(|(replicate_id, ticket)| WalkerData {
@@ -29,29 +39,35 @@ pub trait TDMC: Sized {
             })
             .collect();
 
+        // Perform ticketed DMC sampling for n_dmc_steps steps.
         for i in 0..n_dmc_steps {
+            // At every step, advance and adjust the copy number of each walker.
+            // Iterate over all walkers starting from the first.
             let mut j = 0;
             while j < walker_list.len() {
-                let current_state = &mut walker_list[j].state;
-                let previous_state = current_state.clone();
-
-                Self::propagate_sample(current_state, i);
+                // Calculate a new state, saving the old & new outside the
+                // list for just a moment.
+                let previous_state = walker_list[j].state.clone();
+                self.propagate_sample(&mut walker_list[j].state, i);
                 n_dynamics_evaluations += 1;
 
-                let step_weight = -Self::chi(&current_state, &previous_state, i).exp();
+                // Calculate the generalized DMC weight for the proposed step.
+                let step_weight = (-Self::chi(&walker_list[j].state, &previous_state, i)).exp();
 
+                // If the weight is lower than the walker's ticket, delete the walker
+                // and advance to the next.
                 if step_weight < walker_list[j].ticket {
                     // TODO this is unnecessary, should just be an option
-                    walker_list.remove(0);
+                    walker_list.remove(j);
                 } else {
+                    //                    dbg!(step_weight);
                     let replicate_id = walker_list[j].replicate_id;
                     let n_clones_needed = 1.max((step_weight + uniform.sample(&mut rng)) as i32);
 
                     walker_list[j].ticket /= step_weight;
 
-                    let new_ticket_dist = Uniform::new_inclusive(1.0 / step_weight, 1.0);
-
-                    for _ in 0..n_clones_needed {
+                    for _ in 1..n_clones_needed {
+                        let new_ticket_dist = Uniform::new_inclusive(1.0 / step_weight, 1.0);
                         let cloned_walker = WalkerData {
                             state: walker_list[j].state.clone(),
                             ticket: new_ticket_dist.sample(&mut rng),
@@ -61,11 +77,16 @@ pub trait TDMC: Sized {
                         j += 1;
                         walker_list.insert(j, cloned_walker);
                     }
+                    j += 1
                 }
             }
         }
 
-        println!("used {}", n_dynamics_evaluations);
+        println!(
+            "Completed TDMC in {} milliseconds using {} evaluations of sampling dynamics.",
+            (Instant::now() - start_clock).as_millis(),
+            n_dynamics_evaluations
+        );
         walker_list
             .into_iter()
             .map(|walker| {
